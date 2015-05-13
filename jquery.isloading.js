560b48157e611ccc8a7d834020b187f3dea70dbe
lrz-hal <martin.scholz83@outlook.com>
2015-05-13 14:48:59 +0200

    Add transparency for overlaying

--- jquery.isloading.js
+++ jquery.isloading.js
@@ -1,10 +1,10 @@
 /**
 * Loading plugin for jQuery
 * version: v1.0.6
-* 
-* Small helper to give the user a visual feedback that something is happening 
+*
+* Small helper to give the user a visual feedback that something is happening
 * when fetching/posting data
-* 
+*
 * USAGE:
 * - global overlay:                     $.isLoading();
 * - use javascript:                     $( selector ).isLoading();
@@ -14,21 +14,21 @@
 * @author Laurent Blanes <laurent.blanes@gmail.com>
 * ---
 * Copyright 2013, Laurent Blanes ( https://github.com/hekigan/is-loading )
-* 
+*
 * The MIT License (MIT)
-* 
+*
 * Copyright (c) 2013 Laurent Blanes
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -45,6 +45,7 @@
             'position': "right",        // right | inside | overlay
             'text': "",                 // Text to display next to the loader
             'class': "icon-refresh",    // loader CSS class
+            'transparency': 0.5,        // background transparency for using with overlay
             'tpl': '<span class="isloading-wrapper %wrapper%">%text%<i class="%class% icon-spin"></i></span>',    // loader base Tag
             'disableSource': true,      // true | false
             'disableOthers': []
@@ -53,7 +54,7 @@
     // The actual plugin constructor
     function Plugin( element, options ) {
         this.element = element;
-        
+
         // Merge user options with default ones
         this.options = $.extend( {}, defaults, options );
 
@@ -73,11 +74,11 @@
             };
         }
     }
-    
+
     Plugin.prototype = {
-        
+
         init: function() {
-            
+
             if( $( this.element ).is( "body") ) {
                 this.options.position = "overlay";
             }
@@ -91,7 +92,7 @@
             tpl = tpl.replace( '%class%', self.options['class'] );
             tpl = tpl.replace( '%text%', ( self.options.text !== "" ) ? self.options.text + ' ' : '' );
             self._loader = $( tpl );
-            
+
             // Disable the element
             if( $( self.element ).is( "input, textarea" ) && true === self.options.disableSource ) {
 
@@ -115,7 +116,7 @@
                     var $wrapperTpl = null;
 
                     if( $( self.element ).is( "body") ) {
-                        $wrapperTpl = $('<div class="isloading-overlay" style="position:fixed; left:0; top:0; z-index: 10000; background: rgba(0,0,0,0.5); width: 100%; height: ' + $(window).height() + 'px;" />');
+                        $wrapperTpl = $('<div class="isloading-overlay" style="position:fixed; left:0; top:0; z-index: 10000; background: rgba(0,0,0,' + this.options.transparency + '); width: 100%; height: ' + $(window).height() + 'px;" />');
                         $( "body" ).prepend( $wrapperTpl );
 
                         $( window ).on('resize', function() {
@@ -196,7 +197,7 @@
             });
         }
     };
-    
+
     // Constructor
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
@@ -210,7 +211,7 @@
             }
         });
     };
-    
+
     contruct();
 
 })( jQuery, window, document );
