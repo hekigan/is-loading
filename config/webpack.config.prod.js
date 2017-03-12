e2188370f5d95a9d50d00829d02b9bb1eebaece9
Laurent Blanes <laurent.blanes@gmail.com>
2017-03-13 00:14:16 +0900

    - new version (yay!)

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+module.exports = function (env) {
+    return {
+        plugins: [
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                beautify: false,
+                mangle: {
+                    screw_ie8: true,
+                    keep_fnames: true
+                },
+                compress: {
+                    screw_ie8: true
+                },
+                comments: false
+            })
+        ]
+    };
+};
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+        "postcss-cssnext": {
+            browsers: ['last 2 versions', '> 5%']
+        }
+    }
+};
